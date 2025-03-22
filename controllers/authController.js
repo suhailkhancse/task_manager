b905953099f78b3a7ba8097d7fabb4444b218a90
Suhail Khan <suhail05412@gmail.com>
2025-03-22 21:49:50 +0530

    register code change

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,16 +5,27 @@ const User = require("../models/User");
 exports.register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+
+        // Hash the password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
+        // Create new user
         const newUser = new User({ name, email, password: hashedPassword });
         await newUser.save();
+
         res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
         res.status(500).json({ error: "Registration failed" });
     }
 };
+
 
 exports.login = async (req, res) => {
     try {
